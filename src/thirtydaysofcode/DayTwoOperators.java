b0423c2105792e149e7f03b07181fd0dbf5e1a4c
= <=>
2020-02-07 22:11:33 +0530

    Completed Problem Two

--- src/thirtydaysofcode/DayTwoOperators.java
+++ src/thirtydaysofcode/DayTwoOperators.java
@@ -7,7 +7,13 @@ import java.io.InputStreamReader;
 public class DayTwoOperators {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        double doubleNum = Double.parseDouble(br.readLine());
-        int intNum = Integer.parseInt(br.readLine());
+        double mealCost = Double.parseDouble(br.readLine());
+        int tipPercent = Integer.parseInt(br.readLine());
+        int taxPercent = Integer.parseInt(br.readLine());
+
+        double tip = mealCost * (tipPercent / 100.0);
+        double tax = mealCost * (taxPercent / 100.0);
+        int totalCost = (int) ((int) mealCost + tip + tax);
+        System.out.println(totalCost);
     }
 }
