02be91a53da3efd9c7f01668fbcbde18ec7419d2
= <=>
2020-02-07 22:05:04 +0530

    doing Problem Two

---
+++ src/thirtydaysofcode/DayTwoOperators.java
@@ -0,0 +1,13 @@
+package thirtydaysofcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DayTwoOperators {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double doubleNum = Double.parseDouble(br.readLine());
+        int intNum = Integer.parseInt(br.readLine());
+    }
+}
