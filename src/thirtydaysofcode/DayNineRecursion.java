9a2a2b1a9dde96e8b1663d39d65384587b16b025
= <=>
2020-02-08 10:10:27 +0530

    Completed Problem Nine

---
+++ src/thirtydaysofcode/DayNineRecursion.java
@@ -0,0 +1,21 @@
+package thirtydaysofcode;
+
+import java.util.Scanner;
+
+public class DayNineRecursion {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int fact = factorial(n);
+        System.out.println(fact);
+    }
+
+    static int factorial(int n) {
+
+        if (n == 0 || n == 1)
+            return n;
+        else
+            return (n * factorial(n - 1));
+    }
+}
