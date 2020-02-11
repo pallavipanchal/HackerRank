7cc16a788aa0ccd1425978ec80de6d39646f88a0
= <=>
2020-02-11 20:44:30 +0530

    Simple array - reverse array

---
+++ src/datastructures/array/ArrayDS.java
@@ -0,0 +1,46 @@
+package datastructures.array;
+
+import java.util.Scanner;
+
+public class ArrayDS {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    static int[] reverseArray(int[] a) {
+        int start = 0;
+        int end = a.length - 1;
+        while (start < end) {
+            int temp = a[start];
+            a[start] = a[end];
+            a[end] = temp;
+            start++;
+            end--;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+        int arrCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[arrCount];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arrCount; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] res = reverseArray(arr);
+
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
+
+        System.out.println();
+
+        scanner.close();
+    }
+}
