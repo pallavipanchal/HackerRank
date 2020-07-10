e818c1b1497dc03c1e863c639cb94abe84ff6f02
= <=>
2020-07-11 01:37:41 +0530

    Hello World

---
+++ src/javaPractice/WelcomeToJava.java
@@ -0,0 +1,8 @@
+package javaPractice;
+
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
