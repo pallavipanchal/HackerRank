2f9ec6cbcd60c537ecad97b2f05077cdf5dec783
= <=>
2020-07-12 21:14:11 +0530

    Datatypes

---
+++ src/javaPractice/EndOfFile.java
@@ -0,0 +1,15 @@
+package javaPractice;
+
+import java.util.Scanner;
+
+public class EndOfFile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 1;
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+            System.out.println(count + " " + str);
+            count++;
+        }
+    }
+}
