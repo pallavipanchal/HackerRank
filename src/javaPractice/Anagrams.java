bd8fbb6fe846a9ca4d44294af19655fd1f7dc938
pallavi panchal <panchal.pallavi29@gmail.com>
2020-09-20 00:49:12 +0530

    Anagrams

---
+++ src/javaPractice/Anagrams.java
@@ -0,0 +1,30 @@
+package javaPractice;
+
+import java.util.Scanner;
+
+public class Anagrams {
+    static boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) return false;
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        char[] temp = new char[256];
+        for (int i = 0; i < a.length(); i++) {
+            temp[a.charAt(i)]++;
+            temp[b.charAt(i)]--;
+        }
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i] != 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println((ret) ? "Anagrams" : "Not Anagrams");
+
+    }
+}
